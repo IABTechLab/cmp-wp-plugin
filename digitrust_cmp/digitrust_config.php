7a6950b470812dbb277806b3a5b90a08fc919894
atabbakh <atabbakh@purch.com>
2018-07-13 10:17:20 +0200

    delete global config

--- digitrust_cmp/digitrust_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($_POST['digitrust']['json'])) {
-	$GLOBALS['digitrust_json'] = $_POST['digitrust']['json'];
-}
-?>
-<script type="text/javascript"> 
-	var digitrustConfig = <?php echo ''.$GLOBALS['digitrust_json']; ?>;
-	console.log(digitrustConfig);
-</script>
