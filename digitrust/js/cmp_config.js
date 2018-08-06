2adc41add38b7efe88b2b00cab17b952c523badf
Igor Laktionov <ilaktionov@iponweb.net>
2018-08-06 19:26:11 +0300

    fixed cmp_config

--- digitrust/js/cmp_config.js
+++ digitrust/js/cmp_config.js
@@ -1,1 +1,12 @@
 // This file is required to inject defaultConfig, don't delete it
+var commandQueue = [];
+var cmp = function(command, parameter, callback) {
+  commandQueue.push({
+    command: command,
+    parameter: parameter,
+    callback: callback
+  });
+};
+cmp.commandQueue = commandQueue;
+cmp.config = defaultConfig;
+window.__cmp = cmp;
